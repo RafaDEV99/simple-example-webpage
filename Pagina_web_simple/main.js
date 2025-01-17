383a3c0215c66ee6087378723078507c3238b70d
Your Name <you@example.com>
2025-01-17 16:57:05 +0100

    adding some stuff
    I will add some extra stuff here, and modify the CSS file.
    
    Added a JS file
    
    Yes is a JS file!!
    
    added some new content
    
    Yep in this update is adding new content

---
+++ Pagina_web_simple/main.js
@@ -0,0 +1,23 @@
+function yeet() {
+    var xd = 3;
+    
+
+    console.log("You have been alerted!!");
+    alert("This is cool!!");
+
+    for (var i = 0; i < 20; i++) {
+        console.log(i)
+    }
+
+}
+
+function timerCOOL() {
+    console.log("This is a timer so wait the time!");
+    const timer = setTimeout(() => {
+        console.log("wswkslwkswsl")        
+        alert("You are here!!")
+    }, 1000);
+
+    var numTime = timer;
+    console.log(numTime);
+}
