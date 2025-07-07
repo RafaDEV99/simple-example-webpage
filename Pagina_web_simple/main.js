d6e49bd464629248c058a79be4372d61998c81d8
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-07-07 16:42:45 +0200

    cool a commit!

--- Pagina_web_simple/main.js
+++ Pagina_web_simple/main.js
@@ -20,3 +20,9 @@ function timerCOOL() {
     var numTime = timer;
     console.log(numTime);
 }
+
+function forGo() {
+    for (var i = 0; i < 10; i++) {
+        console.log("This is a for loop: " + i);
+    }
+}
