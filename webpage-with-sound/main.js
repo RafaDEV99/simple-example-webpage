0aca1465c59e3948d6c606ede1629c0849b2b438
RafaDEV99 <162469453+RafaDEV99@users.noreply.github.com>
2025-09-25 17:59:35 +0200

    the same

---
+++ webpage-with-sound/main.js
@@ -0,0 +1,26 @@
+// TODO: Edit this furder
+
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('playSoundButton').addEventListener('click', function () {
+        const trollBody = document.getElementById("troll_body");
+        trollBody.style.display = "grid";
+        trollBody.style.padding = "30px";
+        trollBody.style.visibility = "visible";
+
+        const video = document.getElementById("video");
+        video.currentTime = 0;
+        video.play();
+    });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("StopSoundNOWbutton").addEventListener('click', function () {
+
+        // TODO: and this too xD
+        document.getElementById("troll_body").setAttribute("style", "display: grid; padding: 30px; visibility: hidden")
+        video.pause();
+        video.currentTime = 0.0;
+
+    });    
+});
+
